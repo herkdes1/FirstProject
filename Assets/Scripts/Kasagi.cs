7c5b24e127e5fc3162e94945556b5f9084a9d031
YUSIF\Yufis <yusuf.ose@gmail.com>
2021-01-16 20:08:11 +0300

    Namespace eklendi ve oyun sonu kazanılan para eklendi.

--- Assets/Scripts/Kasagi.cs
+++ Assets/Scripts/Kasagi.cs
@@ -6,125 +6,130 @@ using DG.Tweening;
 using UnityEngine.SceneManagement;
 using Base.Game.Signal;
 using TMPro;
-
-public class Kasagi : MonoBehaviour
+namespace YusufMS
 {
-    Vector3 posVector;
 
-    float state0PosX;
-    static public int state;
-
-    int coin;
-    float coinProgress;
-    public Camera MainCam;
-    public Vector3 CamNewPos;
-    public ParticleSystem smoke;
-    public TMP_Text coinText;
-
-    public Slider progress;
-
-    public GameObject arrow;
-    public GameObject kasagiHairleri;
-    public GameObject horseFrontFurs;
-    bool horseTurn = false;
-    private void Start()
+    public class Kasagi : MonoBehaviour
     {
-        state0PosX = 1f;
-        coin = 0;
-    }
-    void Update()
-    {
-        KasagiHareketAlani();
-    }
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.CompareTag("Horse"))
+        Vector3 posVector;
+
+        float state0PosX;
+        static public int state;
+
+        int coin;
+        float coinProgress;
+        public Camera MainCam;
+        public Vector3 CamNewPos;
+        public ParticleSystem smoke;
+        public TMP_Text coinText;
+        public TMP_Text endScreenCoinText;
+
+        public Slider progress;
+
+        public GameObject arrow;
+        public GameObject kasagiHairleri;
+        public GameObject horseFrontFurs;
+        bool horseTurn = false;
+        private void Start()
         {
-            if (coinProgress > 49.5f)
-            {
-                other.transform.DORotate(new Vector3(0, 90, 0), 0.5f);
-                other.transform.position = Vector3.forward * 4.3f;
-                horseFrontFurs.SetActive(false);
-            }
-            if(coinProgress > 100)
-            {
-                FindObjectOfType<UIManager>().ShowWinScreen();
-            }
+            state0PosX = 1f;
+            coin = 0;
         }
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Clean"))
+        void Update()
         {
-            other.transform.DOLocalRotateQuaternion(Quaternion.Euler(0f, -14f, 74f), 0.5f);
-            other.GetComponent<BoxCollider>().enabled = false;
-            coin += 1;
-            smoke.Play();
-            coinProgress = coin * 0.40f;
-            coinText.text = (coinProgress*3f).ToString("F0");
-            HapticAction.Vibrate();
-
-            progress.value = coinProgress;
-
-            if (!horseTurn && coinProgress > 20)
-            {
-                kasagiHairleri.SetActive(true);
-                horseTurn = !horseTurn;
-            }
-
-            SignalBus<SignalCoinChange, int>.Instance.Fire(1);
+            KasagiHareketAlani();
         }
-    }
-    public void RestartGame()
-    {
-        SceneManager.LoadScene(0);
-    }
-    public void NextLevel()
-    {
-        SignalBus<SignalNextStage>.Instance.Fire();
-    }
-    void BrushActivated()
-    {
-        GetComponent<MeshRenderer>().enabled = true;
-        gameObject.AddComponent<DragObject>();
-    }
-
-    public void KasagiHareketAlani()
-    {
-        posVector = transform.position;
-
-        if(Input.GetMouseButtonDown(0) && state0PosX != 0.3f)
+        private void OnTriggerStay(Collider other)
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit))
+            if (other.CompareTag("Horse"))
             {
-                if (hit.transform.name == "Horse")
+                if (coinProgress > 49.5f)
                 {
-                    MainCam.transform.DOMove(CamNewPos, 1.5f).OnComplete(() => BrushActivated());
-                    arrow.SetActive(false);
-                   // hit.collider.enabled = false;
-                    state0PosX = 0.3f;
+                    other.transform.DORotate(new Vector3(0, 90, 0), 0.5f);
+                    other.transform.position = Vector3.forward * 4.3f;
+                    horseFrontFurs.SetActive(false);
+                }
+                if (coinProgress > 100)
+                {
+                    FindObjectOfType<UIManager>().ShowWinScreen();
                 }
             }
         }
-
-        posVector.y = Mathf.Clamp(posVector.y, 0.85f, 1.15f);
-        if (state0PosX == 0.3f)
-            posVector.x = Mathf.Clamp(posVector.x, -0.21f, 0.41f);
-
-        posVector.z = 4f;
-
-        if(transform.position.x >= 0)
+        private void OnTriggerEnter(Collider other)
         {
-            transform.DORotate(new Vector3(0, 60, -90), 0);
+            if (other.CompareTag("Clean"))
+            {
+                other.transform.DOLocalRotateQuaternion(Quaternion.Euler(0f, -14f, 74f), 0.5f);
+                other.GetComponent<BoxCollider>().enabled = false;
+                coin += 1;
+                smoke.Play();
+                coinProgress = coin * 0.40f;
+                coinText.text = (coinProgress * 3f).ToString("F0");
+                endScreenCoinText.text = (coinProgress * 3f).ToString("F0");
+                HapticAction.Vibrate();
+
+                progress.value = coinProgress;
+
+                if (!horseTurn && coinProgress > 20)
+                {
+                    kasagiHairleri.SetActive(true);
+                    horseTurn = !horseTurn;
+                }
+
+                SignalBus<SignalCoinChange, int>.Instance.Fire(1);
+            }
         }
-        else
+        public void RestartGame()
         {
-            transform.DORotate(new Vector3(0, 120, -90), 0);
+            SceneManager.LoadScene(0);
+        }
+        public void NextLevel()
+        {
+            SignalBus<SignalNextStage>.Instance.Fire();
+        }
+        void BrushActivated()
+        {
+            GetComponent<MeshRenderer>().enabled = true;
+            gameObject.AddComponent<DragObject>();
         }
 
-        transform.position = posVector;
+        public void KasagiHareketAlani()
+        {
+            posVector = transform.position;
+
+            if (Input.GetMouseButtonDown(0) && state0PosX != 0.3f)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.transform.name == "Horse")
+                    {
+                        MainCam.transform.DOMove(CamNewPos, 1.5f).OnComplete(() => BrushActivated());
+                        arrow.SetActive(false);
+                        // hit.collider.enabled = false;
+                        state0PosX = 0.3f;
+                    }
+                }
+            }
+
+            posVector.y = Mathf.Clamp(posVector.y, 0.85f, 1.15f);
+            if (state0PosX == 0.3f)
+                posVector.x = Mathf.Clamp(posVector.x, -0.21f, 0.41f);
+
+            posVector.z = 4f;
+
+            if (transform.position.x >= 0)
+            {
+                transform.DORotate(new Vector3(0, 60, -90), 0);
+            }
+            else
+            {
+                transform.DORotate(new Vector3(0, 120, -90), 0);
+            }
+
+            transform.position = posVector;
+        }
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,27 +2,32 @@
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class UIManager : MonoBehaviour
+namespace YusufMS
 {
-    public GameObject endScreen;
-    public GameObject startScreen;
-    public GameObject horseFurs;
 
-    private void Awake()
+    public class UIManager : MonoBehaviour
     {
-        Time.timeScale = 0;
+        public GameObject endScreen;
+        public GameObject startScreen;
+        public GameObject horseFurs;
+
+        private void Awake()
+        {
+            Time.timeScale = 0;
+        }
+
+        public void ShowWinScreen()
+        {
+            endScreen.SetActive(true);
+            horseFurs.SetActive(false);
+            Time.timeScale = 0;
+        }
+
+        public void StartButton()
+        {
+            startScreen.SetActive(false);
+            Time.timeScale = 1;
+        }
     }
 
-    public void ShowWinScreen()
-    {
-        endScreen.SetActive(true);
-        horseFurs.SetActive(false);
-        Time.timeScale = 0;
-    }
-
-    public void StartButton()
-    {
-        startScreen.SetActive(false);
-        Time.timeScale = 1;
-    }
 }
